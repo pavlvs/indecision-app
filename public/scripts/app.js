75823a14517f8fd1cf123931451a7cbc6934fbfa
Paul J. Shaw <pavlvsxavier@gmail.com>
2021-04-01 06:25:44 +0100

    using JEX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,52 @@
 
 console.log('App.js is running');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'The Ultimate React App'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
+);
+
+var user = {
+    name: 'Paul',
+    age: 55,
+    location: 'London'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 console.log(`App.js is running`)
 
-var template = <h1>Indecision App</h1>
+var app = {
+    title: 'Indecision App',
+    subtitle: 'The Ultimate React App'
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+)
+
+var user = {
+    name: 'Paul',
+    age: 55,
+    location: 'London'
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
