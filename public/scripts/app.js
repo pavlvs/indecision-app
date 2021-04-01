8e6c66beaa50b222dad49ef9c7e0a7195692b6eb
Paul J. Shaw <pavlvsxavier@gmail.com>
2021-04-01 18:12:21 +0100

    manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,38 +22,50 @@ var template = React.createElement(
     )
 );
 
-var user = {
-    name: 'Paul',
-    age: 55,
-    location: 'London'
-};
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-}
+var count = 0;
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,35 +1,44 @@
 console.log(`App.js is running`)
 
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'The Ultimate React App'
 }
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         <p>{app.subtitle}</p>
     </div>
 )
 
-var user = {
-    name: 'Paul',
-    age: 55,
-    location: 'London'
+let count = 0
+
+const addOne = () => {
+    count++
+    renderCounterApp()
 }
-function getLocation (location) {
-    if (location) {
-        return <p>Location: {location}</p>
-    }
+const minusOne = () => {
+    count--
+    renderCounterApp()
+}
+const reset = () => {
+    count = 0
+    renderCounterApp()
 }
 
-var templateTwo = (
-    <div>
-        <h1>{user.name}</h1>
-        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-)
-var appRoot = document.getElementById('app')
+const appRoot = document.getElementById('app')
 
-ReactDOM.render(templateTwo, appRoot)
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+    ReactDOM.render(templateTwo, appRoot)
+}
+
+renderCounterApp()
