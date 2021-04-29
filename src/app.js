bbfbaed284d7addeca562a1cee620883483a1242
pavlvs <pavlvsxavier@gmail.com>
2021-04-29 16:36:33 +0100

    added bind to handleRemoveAll in the Options constructor

--- src/app.js
+++ src/app.js
@@ -39,8 +39,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleRemoveAll = this.handleRemoveAll.bind(this)
+    }
+
     handleRemoveAll() {
-        alert('removeAll')
+        console.log(this.props.options)
     }
     render() {
         return (
